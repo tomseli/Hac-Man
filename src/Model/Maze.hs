e13622439149a8523045d1067e964be583579d12
Tom Selier <tbj.selier@gmail.com>
2024-10-15 15:29:29 +0200

    implemented a test maze builder

--- src/Model/Maze.hs
+++ src/Model/Maze.hs
@@ -1,48 +1,50 @@
 module Model.Maze where
 
 import qualified Data.Map as Map
+import GHC.Conc.Sync (newTVar)
 
-data CornerOrientation = SE | NW | NE | SW
-data WallOrientation = Horizontal | Vertical
+data CornerOrientation = SE | NW | NE | SW deriving (Show)
 
-data WallShape = MkCorner CornerOrientation
-               | MkWallShape WallOrientation
+data WallOrientation = Horizontal | Vertical deriving (Show)
 
-data ConsumableType = Pellet | SuperPellet | Cherry
+data WallShape
+  = MkCorner CornerOrientation
+  | MkWallShape WallOrientation
+  deriving (Show)
 
-data FloorType = MkConsumable ConsumableType | EmptyTile
-data Tile = MkFloor FloorType | MkWall WallShape
+data ConsumableType = Pellet | SuperPellet | Cherry deriving (Show)
 
-type TilePosition = (Int , Int)
-type Maze = Map.Map TilePosition Tile 
+data FloorType = MkConsumable ConsumableType | EmptyTile deriving (Show)
 
+data Tile = MkFloor FloorType | MkWall WallShape deriving (Show)
+
+type TilePosition = (Int, Int)
+
+type Maze = Map.Map TilePosition Tile
+
+-- Constructs a text maze with x*x size
+-- does this function have redundant guards? yes
+-- am i going to make a function purely for testing pretty? no
+-- the maze should have the shape listed below
 {-
 XXXX
 X  X
 X  X
 XXXX
 -}
-
--- Constructs a text maze with x*x size
--- does this function have redundant guards? yes
--- am i going to make a function purely for testing pretty? no
 testMaze :: Int -> Maze
-testMaze size = go 0 Map.empty
-  where go x m | x >= (size - 1)             = m         -- base case
-               | snd coord == 0              = go (x+1) (Map.insert coord wallH m) -- top row
-               | snd coord == (^!) size - 1  = go (x+1) (Map.insert coord wallH m) -- bottom row
-               | fst coord == 0              = go (x+1) (Map.insert coord wallV m) -- left column
-               | fst coord == (^!) size - 1  = go (x+1) (Map.insert coord wallV m) -- right column
-               | otherwise                   = go (x+1) (Map.insert coord empty m) -- everything else
-               where 
-                  coord = (x `mod` (^!) size, size `div` (^!) size)
-                  wallH = MkWall (MkWallShape Horizontal)
-                  wallV = MkWall (MkWallShape Vertical)
-                  empty = MkFloor EmptyTile
+testMaze n = go 0 Map.empty
+  where
+    go x m
+      | x >= n * n          = m -- base case
+      | snd coord == 0     = go (x + 1) (Map.insert coord wallH m) -- top row
+      | snd coord == n - 1 = go (x + 1) (Map.insert coord wallH m) -- bottom row
+      | fst coord == 0     = go (x + 1) (Map.insert coord wallV m) -- left column
+      | fst coord == n - 1 = go (x + 1) (Map.insert coord wallV m) -- right column
+      | otherwise          = go (x + 1) (Map.insert coord empty m) -- everything else
+      where
+        coord = (x `mod` n, x `div` n)
+        wallH = MkWall (MkWallShape Horizontal)
+        wallV = MkWall (MkWallShape Vertical)
+        empty = MkFloor EmptyTile
 
--- yoinked from
--- https://wiki.haskell.org/Generic_number_type#squareRoot
-(^!) :: Int -> Int
-(^!) n = try 0 
-  where try i | i*i <= n    = try (i + 1) 
-              | otherwise   = i - 1
